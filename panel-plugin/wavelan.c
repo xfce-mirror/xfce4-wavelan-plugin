c0033c32ec28bbdd5f735f9b52d212e881eb2219
Andre Miranda <andreldm@xfce.org>
2019-10-19 19:08:13 -0300

    Fix build with panel 4.15

--- panel-plugin/wavelan.c
+++ panel-plugin/wavelan.c
@@ -32,7 +32,7 @@
 
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4panel/xfce-panel-plugin.h>
+#include <libxfce4panel/libxfce4panel.h>
 
 #include "wi.h"
 
