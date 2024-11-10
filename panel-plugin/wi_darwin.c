e133472fa85193b4197e16821efd56fa5cd66745
Torrekie <31795847+Torrekie@users.noreply.github.com>
2024-11-10 15:17:36 +0800

    typo

--- panel-plugin/wi_darwin.c
+++ panel-plugin/wi_darwin.c
@@ -64,18 +64,18 @@ struct wi_device {
   int socket;
 };
 
-static int _wi_carrier(const struct wi_device *);
-static int _wi_getval(const struct wi_device *, struct apple80211req *);
+static int _wi_carrier(const struct wi_device*);
+static int _wi_getval(const struct wi_device*, struct apple80211req*);
 
-static int _wi_netname(const struct wi_device *, char *, size_t);
-static int _wi_quality(const struct wi_device *, int *);
-static int _wi_rate(const struct wi_device *, int *);
+static int _wi_netname(const struct wi_device*, char*, size_t);
+static int _wi_quality(const struct wi_device*, int*);
+static int _wi_rate(const struct wi_device*, int*);
 
-struct wi_device *wi_open(const char *interface) {
-  struct wi_device *device = NULL;
+struct wi_device* wi_open(const char* interface) {
+  struct wi_device* device = NULL;
 
   if (interface != NULL) {
-    if ((device = (struct wi_device *)calloc(1, sizeof(*device))) != NULL) {
+    if ((device = (struct wi_device*)calloc(1, sizeof(*device))) != NULL) {
       strlcpy(device->interface, interface, WI_MAXSTRLEN);
 
       if ((device->socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -88,26 +88,26 @@ struct wi_device *wi_open(const char *interface) {
   return (device);
 }
 
-void wi_close(struct wi_device *device) {
+void wi_close(struct wi_device* device) {
   if (device != NULL) {
     close(device->socket);
     free(device);
   }
 }
 
-int wi_query(struct wi_device *device, struct wi_stats *stats) {
+int wi_query(struct wi_device* device, struct wi_stats* stats) {
   int result;
 
   if (device == NULL || stats == NULL)
     return (WI_INVAL);
 
   /* clear stats first */
-  bzero((void *)stats, sizeof(*stats));
+  bzero((void*)stats, sizeof(*stats));
 
   strlcpy(stats->ws_qunit, "%", 2);
 
-  /* I believe we are able to get vendor by fetching IEEE OUI, but is is more
-   * likely accquiring native informations */
+  /* I believe we are able to get vendor by fetching IEEE OUI, but it is more
+   * likely accquiring native hardware informations */
   strlcpy(stats->ws_vendor, _("Unknown"), WI_MAXSTRLEN);
 
   /* check carrier */
@@ -132,10 +132,10 @@ int wi_query(struct wi_device *device, struct wi_stats *stats) {
   }
 }
 
-static int _wi_carrier(const struct wi_device *device) {
+static int _wi_carrier(const struct wi_device* device) {
   struct ifmediareq ifmr;
 
-  bzero((void *)&ifmr, sizeof(ifmr));
+  bzero((void*)&ifmr, sizeof(ifmr));
   strlcpy(ifmr.ifm_name, device->interface, sizeof(ifmr.ifm_name));
 
   if (ioctl(device->socket, SIOCGIFMEDIA, &ifmr) < 0) {
@@ -160,8 +160,8 @@ static int _wi_carrier(const struct wi_device *device) {
   return ((ifmr.ifm_status & IFM_ACTIVE) != 0 ? WI_OK : WI_NOCARRIER);
 }
 
-static int _wi_getval(const struct wi_device *device,
-                      struct apple80211req *areq) {
+static int _wi_getval(const struct wi_device* device,
+                      struct apple80211req* areq) {
   strlcpy(areq->if_name, device->interface, IFNAMSIZ);
 
   if (ioctl(device->socket, SIOCGA80211, areq) < 0)
@@ -170,12 +170,13 @@ static int _wi_getval(const struct wi_device *device,
   return (WI_OK);
 }
 
-static int _wi_netname(const struct wi_device *device, char *buffer,
+static int _wi_netname(const struct wi_device* device,
+                       char* buffer,
                        size_t len) {
   struct apple80211req areq;
   int result;
 
-  bzero((void *)&areq, sizeof(areq));
+  bzero((void*)&areq, sizeof(areq));
   areq.type = APPLE80211_IOC_SSID;
   areq.len = APPLE80211_MAX_SSID_LEN;
   areq.data = buffer;
@@ -186,13 +187,13 @@ static int _wi_netname(const struct wi_device *device, char *buffer,
   return (WI_OK);
 }
 
-static int _wi_quality(const struct wi_device *device, int *quality) {
+static int _wi_quality(const struct wi_device* device, int* quality) {
   struct apple80211req areq;
   struct apple80211_rssi_data rssi;
   int result;
 
-  bzero((void *)&areq, sizeof(areq));
-  bzero((void *)&rssi, sizeof(rssi));
+  bzero((void*)&areq, sizeof(areq));
+  bzero((void*)&rssi, sizeof(rssi));
 
   areq.type = APPLE80211_IOC_RSSI;
   areq.len = sizeof(rssi);
@@ -201,8 +202,8 @@ static int _wi_quality(const struct wi_device *device, int *quality) {
   if ((result = _wi_getval(device, &areq)) != WI_OK)
     return (result);
 
-  /* areq.rssi gives dBm value, OpenBSD impl gives dBm too.
-   * so use same method to convert dBm to %
+  /* areq.rssi gives dBm value, OpenBSD nr.nr_rssi gives dBm too.
+   * But OpenBSD impl returns %, so use same method to convert dBm to %
    */
 
   /* There may have multiple radios, but Darwin already calculated
@@ -218,11 +219,11 @@ static int _wi_quality(const struct wi_device *device, int *quality) {
   return (WI_OK);
 }
 
-static int _wi_rate(const struct wi_device *device, int *rate) {
+static int _wi_rate(const struct wi_device* device, int* rate) {
   struct apple80211req areq;
   int result;
 
-  bzero((void *)&areq, sizeof(areq));
+  bzero((void*)&areq, sizeof(areq));
 
   areq.type = APPLE80211_IOC_RATE;
 
